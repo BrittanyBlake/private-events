d8ece1b03ffaafe9c8b8514fcb8f1403f621b507
adam <aabou9@gmail.com>
2020-06-15 16:00:14 +0100

    add session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(username:params[:session][:username].downcase)
+        if user
+            session[:user_id] = user.id
+            flash[:notice] = "welcome #{user.username}. You have been logged in"
+            redirect_to user
+        else
+            flash[:alert] = "something went wrong"
+            render 'new'
+        end
     end
 
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "You've been logged out"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create, :new, :show]
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
