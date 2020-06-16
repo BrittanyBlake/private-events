5c4e2cfae762d857e79fbf427bcadbe142892eaa
Brittany Blake <brittany.blake49@gmail.com>
2020-06-16 11:13:16 +0200

    build event model and add associaation with creator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_134728) do
+ActiveRecord::Schema.define(version: 2020_06_16_090010) do
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "creator_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
