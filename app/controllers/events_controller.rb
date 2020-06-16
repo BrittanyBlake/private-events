a04c8b39add9b2933ed21646421b3e00b02bce7d
Brittany Blake <brittany.blake49@gmail.com>
2020-06-16 11:59:41 +0200

    Create events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,28 @@
+class EventsController < ApplicationController
+    def index
+    end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.events.build(event_params)
+        if @event.save
+            flash[:notice] = "Event was created successfully"
+            redirect_to @event
+        else
+            flash.now[:alert] = "something went wrong"
+            render 'new'
+        end
+    end
+
+     private 
+    def event_params
+        params.require(:event).permit(:title, :description, :date)
+    end
+end
