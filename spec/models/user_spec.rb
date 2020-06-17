522aa049999d63cc74d4cccaf5a75c8124387cf8
Brittany Blake <brittany.blake49@gmail.com>
2020-06-17 11:30:01 +0200

    Add test for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+
+RSpec.describe User, type: :model do
+  context "validations" do
+      subject do 
+        described_class.new( username: "Jane" ) 
+      end
+
+      it 'expects username to be valid' do
+        expect(subject).to be_valid
+      end
+
+      it 'is not valid without a username' do
+        subject.username = nil
+        expect(subject).to_not be_valid
+      end
+  end
+
+  context "associations" do
+    it 'should have many events' do
+      t = User.reflect_on_association(:events)
+      expect(t.macro).to eq(:has_many)
+    end
+
+    it 'should have many attended events' do
+      t = User.reflect_on_association(:attended_events)
+      expect(t.macro).to eq(:has_many)
+    end
+
+    it 'should have many attending events' do
+      t = User.reflect_on_association(:attending_events)
+      expect(t.macro).to eq(:has_many)
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,7 +27,7 @@ require 'rspec/rails'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 
-require 'support/database_cleaner'
+# require 'support/database_cleaner'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
