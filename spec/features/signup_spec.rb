acbc56a24106bd873ee45891bd37bc510192f782
Brittany Blake <brittany.blake49@gmail.com>
2020-06-17 13:42:17 +0200

    Add tests for event management

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature 'Sign up', type: :feature do
+    scenario "Has a sign up page" do
+        visit signup_url
+        expect(page).to have_content 'sign up'
+    end
+
+    scenario "Sign up a new user" do
+        visit signup_url
+        fill_in 'username', with: 'jane'
+        click_button('sign up')
+        expect(page).to have_content 'jane'
+    end
+
+   
+end
