bd2c63ff2fdeb303564a8a8a454edd96e2ccb710
Brittany Blake <brittany.blake49@gmail.com>
2020-06-17 12:05:42 +0200

    Add rspec for attended events model

---
+++ spec/models/attended_event_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe AttendedEvent, type: :model do
+  context "Associations" do
+    it 'should belong to an event' do
+      t = AttendedEvent.reflect_on_association(:event)
+      expect(t.macro).to eq(:belongs_to)
+    end
+
+    it 'should belong to an attendee' do
+      t = AttendedEvent.reflect_on_association(:attendee)
+      expect(t.macro).to eq(:belongs_to)
+    end
+  end
+  
+end
